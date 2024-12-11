e2c6c863a090eb1b8af7692ce7c9c36f84d4f635
JeongminSeon <46957316+JeongminSeon@users.noreply.github.com>
2024-12-11 13:07:13 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,5 @@
+function solution(my_string, num1, num2) {
+    let str = Array.from(my_string);
+    [str[num1], str[num2]] = [str[num2], str[num1]];
+    return str.reduce((acc, char) => acc + char, '');
+}
