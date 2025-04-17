46820957efe9e9d4925d8d090e3dab29525fc30d
JeongminSeon <46957316+JeongminSeon@users.noreply.github.com>
2025-04-18 01:10:19 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,19 +1,10 @@
 function solution(s) {
-    let answer = "";
-    let arr = s.split(" ");
-    
-    for(let i = 0; i < arr.length; i++) {
-        let str = Array.from(arr[i]);
-        for(let j = 0; j < str.length; j++) {
-            if(j % 2 === 0) {
-                answer += str[j].toUpperCase(); 
-            } else {
-                answer += str[j].toLowerCase();
-            }
-            
-        }
-        if(i !== arr.length - 1) answer += " ";
-    }
-    
-    return answer;
+    return s.split(" ")
+    .map(word => 
+        word
+            .split("")
+            .map((ch,idx) => idx % 2 === 0 ? ch.toUpperCase() : ch.toLowerCase())
+            .join("")
+    )
+    .join(" ");
 }
