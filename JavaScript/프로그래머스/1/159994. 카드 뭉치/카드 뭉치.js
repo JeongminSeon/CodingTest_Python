390416587f1350fb449bd1262c25d94a00f2654c
JeongminSeon <46957316+JeongminSeon@users.noreply.github.com>
2025-05-01 00:10:56 +0900

    [level 1] Title: 카드 뭉치, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ JavaScript/프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,13 +1,11 @@
 function solution(cards1, cards2, goal) {
     let i = 0, j = 0, k = 0;
     
-    while(k !== goal.length) {
+    for(let k = 0; k < goal.length; k++) {
         if(cards1[i] === goal[k]) {
             i++;
-            k++;
         } else if (cards2[j] === goal[k]) {
             j++;
-            k++;
         } else {
             return "No"
         }
