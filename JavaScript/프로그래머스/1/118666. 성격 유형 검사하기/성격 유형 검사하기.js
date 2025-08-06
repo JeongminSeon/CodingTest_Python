075c1f2a33436ce6fec68a640443aa284d208470
JeongminSeon <46957316+JeongminSeon@users.noreply.github.com>
2025-08-06 20:48:17 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.52 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ JavaScript/프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,35 +1,21 @@
 function solution(survey, choices) {
-    let answer = '';
-    let N = survey.length;
+    const types = ['RT', 'CF', 'JM', 'AN'];
+    const MBTI = {};
     
-    let hash = {
-        'R' : 0,
-        'T' : 0,
-        'C' : 0,
-        'F' : 0,
-        'J' : 0,
-        'M' : 0,
-        'A' : 0,
-        'N' : 0,
-    }; // 해쉬 테이블 생성
+    types.forEach((type) => {
+        type.split("").map((t) => MBTI[t] = 0)
+    })
     
-    for(let i = 0; i < N; i++) {
-        let first_letter = survey[i].slice(0,1);
-        let second_letter = survey[i].slice(1);
+    choices.forEach((choice, index) => {
+        const [a, b] = survey[index];
         
-        if(choices[i] > 4){
-            hash[second_letter] = (hash[second_letter] || 0) + choices[i] % 4;
-        } else if(choices[i] < 4) {
-            hash[first_letter] = (hash[first_letter] || 0) + 4 - choices[i];
+        if(choice > 4) {
+            MBTI[b] = MBTI[b] + Math.abs(4 - choice)    
         } else {
-            continue;
+            MBTI[a] = MBTI[a] + 4 - choice;
         }
-    }
+    })
     
-    hash['R'] >= hash['T'] ? answer += 'R' : answer += 'T';
-    hash['C'] >= hash['F'] ? answer += 'C' : answer += 'F';
-    hash['J'] >= hash['M'] ? answer += 'J' : answer += 'M';
-    hash['A'] >= hash['N'] ? answer += 'A' : answer += 'N';
     
-    return answer;
+    return types.map(([a,b]) => MBTI[a] >= MBTI[b] ? a : b).join("");
 }
