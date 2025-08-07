06c2334b0f1e87cfe0a9686b1b720f094d3a26a0
JeongminSeon <46957316+JeongminSeon@users.noreply.github.com>
2025-08-07 16:39:10 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.44 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
+++ JavaScript/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -1,18 +1,17 @@
 function solution(wallpaper) {
+    let lux = Infinity, luy = Infinity;
+    let rdx = -1, rdy = -1; 
     
-    let xPos = [];
-    let yPos = [];
     for(let i = 0; i < wallpaper.length; i++) {
         for(let j = 0; j < wallpaper[i].length; j++) {
             if(wallpaper[i][j] === '#') {
-                xPos.push(j); // 열 (X축)
-                yPos.push(i); // 행 (Y축)
+                lux = Math.min(lux, i);
+                luy = Math.min(luy, j);
+                rdx = Math.max(rdx, i + 1);
+                rdy = Math.max(rdy, j + 1);
             }
         }
     }
     
-    xPos.sort((a, b) => a - b);
-    yPos.sort((a, b) => a - b);
-    
-    return [yPos[0], xPos[0], yPos[yPos.length - 1] + 1, xPos[xPos.length - 1] + 1]
+    return [lux, luy, rdx, rdy]
 }
