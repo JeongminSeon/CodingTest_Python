a1315b0a7354fcac8ca8bf41f2db9a627434cb94
JeongminSeon <46957316+JeongminSeon@users.noreply.github.com>
2025-09-02 00:20:55 +0900

    [level 2] Title: 카펫, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42842. 카펫/카펫.js
+++ JavaScript/프로그래머스/2/42842. 카펫/카펫.js
@@ -1,15 +1,4 @@
 function solution(brown, yellow) {
-    let answer = [];
-    
-    const total_tiles = brown + yellow;
-    
-    for(let height = 3; height <= total_tiles / 2; height++) {
-        if(total_tiles % height !== 0) continue;
-        
-        let width = total_tiles / height;
-        
-        if(height > width) continue;
-        
-        if( (width - 2) * (height - 2) === yellow) return [width, height];
-    }
+    var answer = [];
+    return answer;
 }
