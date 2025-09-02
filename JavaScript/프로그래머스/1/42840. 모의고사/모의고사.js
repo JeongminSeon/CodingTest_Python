144047843c26a31cfb6c172b64303839a21a725f
JeongminSeon <46957316+JeongminSeon@users.noreply.github.com>
2025-09-02 10:58:42 +0900

    [level 1] Title: 모의고사, Time: 5.13 ms, Memory: 36.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/42840. 모의고사/모의고사.js
+++ JavaScript/프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,26 +1,27 @@
 function solution(answers) {
     let count1 = 0, count2 = 0, count3 = 0;
     
-    let ans1 = [1,2,3,4,5]; // 길이 5
-    let ans2 = [2,1,2,3,2,4,2,5]; // 길이 8
-    let ans3 = [3,3,1,1,2,2,4,4,5,5]; // 길이 10
+    let ans1 = [1,2,3,4,5]; // 5
+    let ans2 = [2,1,2,3,2,4,2,5]; // 8
+    let ans3 = [3,3,1,1,2,2,4,4,5,5]; // 10
     
-    answers.forEach((ans,idx) => {
-        if(ans === ans1[idx % 5]) {
+    for(let i = 0; i < answers.length; i++) {
+        
+        if(answers[i] === ans1[i % 5]) {
             count1++;
         }
-        if(ans === ans2[idx % 8]) {
+        
+        if(answers[i] === ans2[i % 8]) {
             count2++;
         }
-        if(ans === ans3[idx % 10]) {
+        
+        if(answers[i] === ans3[i % 10]) {
             count3++;
         }
-    })
+    }
     
     let scores = [count1, count2, count3];
     let max = Math.max(...scores);
     
-    return scores
-        .map((score, idx) => (score === max ? idx + 1 : null)) 
-        .filter(v => v !== null); // 최대값 아닌애들 제외
+    return scores.map((score, idx) => (score === max ? idx + 1 : null)).filter(v => v !== null);
 }
