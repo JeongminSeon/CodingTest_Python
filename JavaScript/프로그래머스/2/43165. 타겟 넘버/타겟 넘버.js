17c77a67aa2f0732c8b2ac0dd5c5224c10a3200b
JeongminSeon <46957316+JeongminSeon@users.noreply.github.com>
2025-11-09 21:56:09 +0900

    [level 2] Title: 타겟 넘버, Time: 14.91 ms, Memory: 35.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ JavaScript/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,21 +1,19 @@
 function solution(numbers, target) {
     let count = 0;
     
-    function bfs(index, currentSum) {
+    function dfs(index, currentValue) {
         // 1. 종료 조건
         if(index === numbers.length) {
-            if(currentSum === target) count++;
-            return ;
+            if(currentValue === target) count++;
+            
+            return;
         }
         
-        // 2. 이동 규칙 명시
-        // +1, -1을 하면 된다.
-        
-        bfs(index + 1, currentSum + numbers[index]);
-        bfs(index + 1, currentSum - numbers[index]);
+        // 2. 이동 조건
+        dfs(index + 1, currentValue + numbers[index]);
+        dfs(index + 1, currentValue - numbers[index]);
     }
     
-    bfs(0,0);
-    
+    dfs(0, 0);
     return count;
 }
