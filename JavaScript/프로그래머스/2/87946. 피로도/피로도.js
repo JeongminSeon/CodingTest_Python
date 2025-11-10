30cec25f876a7aa38de2c8a0ac65a373176ef0b9
JeongminSeon <46957316+JeongminSeon@users.noreply.github.com>
2025-11-10 18:40:41 +0900

    [level 2] Title: 피로도, Time: 30.81 ms, Memory: 37.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/87946. 피로도/피로도.js
+++ JavaScript/프로그래머스/2/87946. 피로도/피로도.js
@@ -1,37 +1,24 @@
 function solution(k, dungeons) {
-    let route = new Set();
-    let max = 0; // 방문할 수 있는 던전 수
+    let maxDungeons = 0;
+    const visited = new Array(dungeons.length).fill(false);
     
-    function createRoutes(currentRoute, remainPoints) {
+    function dfs(currentK, count) {
+        maxDungeons = Math.max(maxDungeons, count);
         
-        if(currentRoute.length === dungeons.length) {
-            route.add(currentRoute);
-        }
-        
-        for(let i = 0; i < remainPoints.length; i++) {
-            let pointToAttach = remainPoints[i]
-            const nextPoints = [...remainPoints];
-            nextPoints.splice(i, 1);
+        for(let i =0; i < dungeons.length; i++) {
+            const [minHealth, useHealth] = dungeons[i];
             
-            createRoutes([...currentRoute, pointToAttach], nextPoints);
-        }
-    }
-    
-    createRoutes([], dungeons);
-    
-    for(const arr of route) {
-        let count = 0; // 임시 카운터
-        let hp = k;
-        for(const point of arr) {
-            if(hp >= point[0]) {
-                hp -= point[1];
-                count++;
-            } else {
-                break;
+            if(!visited[i] && currentK >= minHealth) {
+                visited[i] = true;
+                
+                dfs(currentK - useHealth, count + 1);
+                
+                visited[i] = false;
             }
         }
-        max = Math.max(count, max);
     }
     
-    return max
+    dfs(k, 0);
+    
+    return maxDungeons;
 }
