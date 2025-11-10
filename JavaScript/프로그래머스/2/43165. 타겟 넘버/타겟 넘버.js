c7f1219c33946290ca976068bb7c8ffa334207c6
JeongminSeon <46957316+JeongminSeon@users.noreply.github.com>
2025-11-10 10:42:19 +0900

    [level 2] Title: 타겟 넘버, Time: 28.49 ms, Memory: 35 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ JavaScript/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,19 +1,16 @@
 function solution(numbers, target) {
-    let count = 0;
+    let answer = 0;
     
     function dfs(index, currentValue) {
-        // 1. 종료 조건
         if(index === numbers.length) {
-            if(currentValue === target) count++;
-            
+            if(currentValue === target) answer++;
             return;
         }
         
-        // 2. 이동 조건
         dfs(index + 1, currentValue + numbers[index]);
         dfs(index + 1, currentValue - numbers[index]);
     }
     
-    dfs(0, 0);
-    return count;
+    dfs(0, 0)
+    return answer;
 }
