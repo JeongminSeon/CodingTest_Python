aa06a0f008499e9f30faf33f934f443f85638b4f
JeongminSeon <46957316+JeongminSeon@users.noreply.github.com>
2025-11-11 10:53:28 +0900

    [level 3] Title: 단어 변환, Time: 0.61 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ JavaScript/프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,4 +1,5 @@
 function solution(begin, target, words) {
+    let answer = 0; 
     const visited = new Array(words.length).fill(false);
     
     const queue = [];
@@ -6,24 +7,26 @@ function solution(begin, target, words) {
     let head = 0;
     
     while(head < queue.length) {
-        const [currentWord, steps] = queue[head];
+        const [currentWord, step] = queue[head];
         head++;
         
-        if(currentWord === target) return steps;
+        if(currentWord === target) {
+            return step;
+        }
         
         for(let i = 0; i < words.length; i++) {
             if(!visited[i] && isOneDiff(currentWord, words[i])) {
                 visited[i] = true;
-                queue.push([words[i], steps + 1]);
+                queue.push([words[i], step + 1]);
             }
         }
     }
     
-    return 0;
+    return answer;
 }
 
 function isOneDiff(word1, word2) {
-    let diff = 0;
+    let diff = 0; 
     
     for(let i = 0; i < word1.length; i++) {
         if(word1[i] !== word2[i]) diff++;
