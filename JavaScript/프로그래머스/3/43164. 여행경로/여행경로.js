a6495743f1bd14ccb937c2154a3efbe5a9cb4854
JeongminSeon <46957316+JeongminSeon@users.noreply.github.com>
2025-11-11 11:07:55 +0900

    [level 3] Title: 여행경로, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/43164. 여행경로/여행경로.js
+++ JavaScript/프로그래머스/3/43164. 여행경로/여행경로.js
@@ -1,31 +1,32 @@
 function solution(tickets) {
-    const graph = {};
     const path = [];
-
+    const graph = {};
+    
     for(const [from, to] of tickets) {
-        if(!graph[from]) {
-            graph[from] = [to];
-        } else {
+        if(graph[from]) {
             graph[from].push(to);
+        } else {
+            graph[from] = [to];
         }
     }
     
-    for(const key in graph) {
-        graph[key].sort();
+    for(const city in graph) {
+        graph[city].sort();
+        graph[city].reverse();
     }
     
     function dfs(city) {
-        let destinations = graph[city];
+        const destinations = graph[city];
         
         while(destinations && destinations.length > 0) {
-            let nextCity = destinations.shift();
+            const nextCity = destinations.pop();
             dfs(nextCity);
         }
         
         path.push(city);
     }
     
-    dfs('ICN')
+    dfs('ICN');
     
     return path.reverse();
 }
