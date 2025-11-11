1e2ff70bc116d62764c11b0787b49027356cdf67
JeongminSeon <46957316+JeongminSeon@users.noreply.github.com>
2025-11-11 12:27:53 +0900

    [level 2] Title: 미로 탈출, Time: 21.13 ms, Memory: 38.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/159993. 미로 탈출/미로 탈출.js
+++ JavaScript/프로그래머스/2/159993. 미로 탈출/미로 탈출.js
@@ -1,61 +1,55 @@
-function solution(maps) {    
+function solution(maps) {
+    let dist;
     let start;
     let lever;
     let exit;
     
-    let dist = -1;
-    maps.forEach((map, xIndex) => {
-        let sIndex = map.indexOf('S');
-        let lIndex = map.indexOf('L');
-        let eIndex = map.indexOf('E');
-        
-        if(sIndex !== -1) {
-            start = [xIndex, sIndex];
+    for(let i = 0; i < maps.length; i++) {
+        for(let j = 0; j < maps[0].length; j++) {
+            if(maps[i][j] === 'S') start = [i,j];
+            if(maps[i][j] === 'L') lever = [i,j];
+            if(maps[i][j] === 'E') exit = [i,j];
         }
-        if (lIndex !== -1) {
-            lever = [xIndex, lIndex];
-        }
-        if (eIndex !== -1) {
-            exit = [xIndex, eIndex];
-        }
-    })
+    }
     
     dist = bfs(start, lever, 0, maps);
     
-    if(dist !== -1) {
-        dist = bfs(lever, exit, dist, maps);
-    }
+    if(dist === -1) return -1;
     
-    return dist;
+    return bfs(lever, exit, dist, maps);
+    
 }
 
 function bfs(start, end, dist, maps) {
-    const rows = maps.length;
+    const rows = maps.length; 
     const cols = maps[0].length;
     const visited = new Array(rows).fill(null).map(() => new Array(cols).fill(false));
     
-    let queue = [[...start, dist]];
-    visited[start[0]][start[1]] = true;
+    const queue = [];
     let head = 0;
+    queue.push([...start,dist]);
+    visited[start[0]][start[1]] = true;
     
     while(head < queue.length) {
-        const [px, py, dist] = queue[head];
+        const [px, py, currentDist] = queue[head];
         head++;
         
-        if(px === end[0] && py === end[1]) {
-            return dist;
+        if(px === end[0] && py === end[1]){
+            return currentDist;
         }
         
         const dx = [1, -1, 0, 0];
-        const dy = [0, 0, 1, -1];
+        const dy = [0, 0, -1, 1];
         
         for(let i = 0; i < 4; i++) {
             const nx = px + dx[i];
             const ny = py + dy[i];
             
-            if(nx >= 0 && nx < rows && ny < cols && ny >= 0 && !visited[nx][ny] && maps[nx][ny] !== 'X') {
-                visited[nx][ny] = true;
-                queue.push([nx, ny, dist + 1]);
+            if(nx >= 0 && nx < rows && ny >= 0 && ny < cols && maps[nx][ny] !== 'X'){
+                if(!visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    queue.push([nx,ny, currentDist+1]);
+                }
             }
         }
     }
